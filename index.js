7e3c927fb93a1a7468630824094764f966ddfe88
KhoaPham <phamdangkhoa0403@gmail.com>
2025-07-17 10:12:23 +0700

    commit message

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ const app = require("./app");
 const express = require("express");
 const path = require("path");
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "..", "frontend", "build", "index.html")
-    );
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API started...");
-  });
-}
-
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(
+//       path.resolve(__dirname, "..", "frontend", "build", "index.html")
+//     );
+//   });
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API started...");
+//   });
+// }
+app.get("/", (req, res) => {
+  res.send("API started...");
+});
 const PORT = 3001;
 
 app.listen(PORT, () => {
