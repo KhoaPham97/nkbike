73e3ef03b91caa8f867ba79173c309d7aa63d600
KhoaPham <phamdangkhoa0403@gmail.com>
2025-07-17 10:09:50 +0700

    commit message

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,12 +4,12 @@ const db =
   "mongodb+srv://khoapham:khoa0403@cluster0.mtfxh4m.mongodb.net/nhatkhang";
 
 mongoose
-  .connect(db)
+  .connect(db, {
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Successfully connected to database");
   })
   .catch((error) => {
     console.log("Could not connect to database", error);
   });
-
-mongoose.set("useCreateIndex", true);
